d4cb20a30da51bceb14e1e65a7a0b8e1e2358962
sreyasas95 <sreyasas95@gmail.com>
2025-09-18 10:48:27 +0200

    Fix on wiki_graph documentation

---
+++ R/wiki_graph.r
@@ -0,0 +1,25 @@
+#' Example weighted graph data frame for Dijkstra's algorithm.
+#' Containing three columns: from node v1, to node v2, and weight w.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{Integer. The starting node.}
+#'   \item{v2}{Integer. The ending node.}
+#'   \item{w}{Numeric. The weight associated with the edge.}
+#' }
+#'
+#' @references
+#' Wikipedia: Dijkstra algorithm
+#' \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'
+#' @examples
+#' data(wiki_graph)
+#'
+#' #Example: run Dijkstra starting from node 1
+#' dijkstra(wiki_graph, 1)
+#'
+#' #Example: run Dijkstra starting from node 3
+#' dijkstra(wiki_graph, 3)
+#'
+#' data(wiki_graph)
+"wiki_graph"
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# group
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of group is to ...
+
+## Installation
+
+You can install the development version of group like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(group)
+## basic example code
+```
+
